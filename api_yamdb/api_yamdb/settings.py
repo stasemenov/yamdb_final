e068e127ccd418e301913bc7fdfb6211640480b6
Станислав Семёнов <leadwebber@gmail.com>
2023-04-16 01:51:08 +0300

    update: settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('TOKEN'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['158.160.40.250']
 
 
 # Application definition
